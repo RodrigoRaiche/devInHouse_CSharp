d307df2543a54f389528e280c424b0a76de64022
rodrigoraiche <rodrigo_raiche@estudante.sc.senai.br>
2022-06-08 20:01:56 -0300

    Add M2S02_Ex3

---
+++ M2S02_Ex3.Console/Program.cs
@@ -0,0 +1,26 @@
+﻿class Calculadora
+{
+    public enum opcoesCalculadora
+    {
+        Sair,
+        Adição,
+        Subtração,
+        Multiplicação,
+        Divisão
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Opções da Calculadora");
+        Console.WriteLine("-----------------------");
+        for (int i = 0; i < 5; i++)
+        {
+           Console.WriteLine($"Opção {i} - {(opcoesCalculadora) i}");
+        }
+        Console.WriteLine("-----------------------");
+        Console.Write("Escolha a opção: ");
+        opcoesCalculadora opcaoEscolhida = (opcoesCalculadora) int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Opção escolhida: {opcaoEscolhida}");
+    }
+}
