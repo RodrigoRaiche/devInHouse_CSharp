2212fb8401456988247c6993690b330b43838a4b
rodrigoraiche <rodrigo_raiche@estudante.sc.senai.br>
2022-06-14 18:56:14 -0300

    Add M2S03_Ex9

---
+++ M2S03_Ex9.console/Calcular.cs
@@ -0,0 +1,8 @@
+public class Calcular{
+  public Calcular(int numero1 = 0,int numero2 = 0, int numero3 = 0, int numero4 = 0){
+      this.Resultado = numero1 + numero2 + numero3 + numero4;
+  }
+
+  public int Resultado {get; set;}
+
+}
---
+++ M2S03_Ex9.console/Program.cs
@@ -0,0 +1,18 @@
+﻿static class Program
+{
+    static void Main(string[] args)
+    {
+        Calcular soma1 = new Calcular(1);
+        Console.WriteLine("A soma dos número é: " + soma1.Resultado);
+
+        Calcular soma2 = new Calcular(1,2);
+        Console.WriteLine("A soma dos número é: " + soma2.Resultado);
+
+        Calcular soma3 = new Calcular(1,3,4);
+        Console.WriteLine("A soma dos número é: " + soma3.Resultado);
+
+        Calcular soma4 = new Calcular(1,4,5,6);
+        Console.WriteLine("A soma dos número é: " + soma4.Resultado);
+
+    }
+}
