b17cf5a2b558d2bf5519e946bca0a9f444345a4b
rodrigoraiche <rodrigo_raiche@estudante.sc.senai.br>
2022-06-28 23:37:56 -0300

    Atualiza Resultado

--- M2S05/Resultado.cs
+++ M2S05/Resultado.cs
@@ -5,14 +5,19 @@ namespace M2S05
         public static string DecideVencedor(Robo robo1, Robo robo2)
         {
             string resultado = "";
+            bool robo1Destruido = robo1.Status == Enums.EStatus.Destruído;
+            bool robo2Vencedor = robo2.PontoVida > robo1.PontoVida;
 
-            if (robo1.Status == Enums.EStatus.Destruído || robo2.PontoVida > robo1.PontoVida)
+            bool robo2Destruido = robo2.Status == Enums.EStatus.Destruído;
+            bool robo1Vencedor = robo1.PontoVida > robo2.PontoVida;
+
+            if (robo1Destruido || robo2Vencedor)
             {
                 resultado = "O vencedor foi o jogador 2.";
             }
             else
             {
-                if (robo2.Status == Enums.EStatus.Destruído || robo1.PontoVida > robo2.PontoVida)
+                if (robo2Destruido || robo1Vencedor)
                 {
                     resultado = "O vencedor foi o jogador 1.";
                 }
